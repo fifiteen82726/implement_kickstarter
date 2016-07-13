caf4a7942bf75f2770f875b7b6c8ba712912d616
fifiteen82726 <fifiteen82726@gmail.com>
2016-07-13 21:10:33 +0800

    add project to category

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'kickscraper', :git => 'git://github.com/markolson/kickscraper.git' 
 gem 'pry-rails'
+gem 'will_paginate'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-
 # name 標題 
 # photo 圖片
 # 集資金額
@@ -15,42 +6,63 @@
 # 贊助人數
 # state 集資狀態(a string of 'live', 'successful', 'failed', 'suspended', 'deleted', or 'canceled')
 
-# name photo   		goal     
-# create_table "projects", force: :cascade do |t|
-#     t.integer  "kickstart_id"
-#     t.string   "name"
-#     t.string   "photo_med"
-#     t.float    "pledged"
-#     t.float    "goal"
-#     t.integer  "backers_count"
-#     t.string   "state"
-#     t.datetime "deadline"
-#     t.datetime "created_at",    null: false
-#     t.datetime "updated_at",    null: false
-#   end
+
 Project.destroy_all
 Category.destroy_all
 
 
-
-
 client = Kickscraper.client
-c = client.recently_launched_projects
-c.each do |project|
-Project.create(kickstart_id: project.id, name: project.name, photo_med: project.photo["med"],
-				pledged: project.pledged, goal: project.goal, backers_count: project.backers_count,
-				state: project.state, deadline: project.deadline)	
-end
 
-# p1  = Project.first
-# p2 = Project.second
-
-
+# Create 分類
 category = client.categories
 
 category.each do |category|
 	Category.create(name: category.name)
+end
+
+
+
+
+#Create Project
+#client = Kickscraper.client
+
+# get latest
+c = client.recently_launched_projects
+c.each do |project|
+
+cate = Category.find_by_name(project.category.name)
+
+
+# project.category
+p = Project.create(kickstart_id: project.id, name: project.name, photo_med: project.photo["med"],
+	pledged: project.pledged, goal: project.goal, backers_count: project.backers_count,
+	state: project.state, deadline: project.deadline)
+
+cate.projects << p 	
+end
+
+
+#再抓一百個
+
+5.times do |i|
+	c = client.load_more_projects if client.more_projects_available?
+	c.each do |project|
+		Project.create(kickstart_id: project.id, name: project.name, photo_med: project.photo["med"],
+			pledged: project.pledged, goal: project.goal, backers_count: project.backers_count,
+			state: project.state, deadline: project.deadline)	
 	end
+end
+
+
+
+
+
+
+
+
+# p1  = Project.first
+# p2 = Project.second
+
 
 
 
